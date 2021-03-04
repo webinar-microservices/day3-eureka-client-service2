744b4bd13afec563b57168e22ada80a4960e8074
Santosh Mondal <santosh.ece06@gmail.com>
2021-03-04 13:17:25 +0530

    lb

--- src/main/java/in/webinar/SecondMicroService.java
+++ src/main/java/in/webinar/SecondMicroService.java
@@ -1,17 +1,32 @@
 package in.webinar;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/service2")
 public class SecondMicroService {
 	
+	@Autowired
+	private RestTemplate restTemplate;
 	
 	@GetMapping("/")
 	public String sayHello() {
 		return "Hello SecondMicroService!!";
+	}
+	
+	
+	@GetMapping("/s1")
+	public String consumerService1() {
+		String str = "Consumer Service1";
+		
+		String url = "lb://SERVICE1/service1/";
+		str = restTemplate.getForObject(url, String.class);
+		
+		return str;
 	}
 
 }
