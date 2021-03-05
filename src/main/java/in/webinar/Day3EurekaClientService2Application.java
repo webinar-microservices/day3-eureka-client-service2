20dc7897812b02b97f900450c80f195128eecc36
Santosh Mondal <santosh.ece06@gmail.com>
2021-03-05 12:49:31 +0530

    service2

--- src/main/java/in/webinar/Day3EurekaClientService2Application.java
+++ src/main/java/in/webinar/Day3EurekaClientService2Application.java
@@ -2,11 +2,22 @@ package in.webinar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
 public class Day3EurekaClientService2Application {
+	
+	
+	// Producing The Object 
+	@Bean
+	@LoadBalanced
+	public RestTemplate registerRestTemplate() {
+		return new RestTemplate();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Day3EurekaClientService2Application.class, args);
--- src/main/java/in/webinar/SecondMicroService.java
+++ src/main/java/in/webinar/SecondMicroService.java
@@ -1,17 +1,60 @@
 package in.webinar;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/service2")
 public class SecondMicroService {
 	
+	// Consuming the object
+	@Autowired
+	private RestTemplate restTemplate;
+	
 	
 	@GetMapping("/")
 	public String sayHello() {
 		return "Hello SecondMicroService!!";
+	}
+	
+	
+	@GetMapping("/v1")
+	public String iAmDependentOnService1() {
+		
+		// OLD APPROACH
+		String url = "http://localhost:8081/service1/";
+		String response = restTemplate.getForObject(url, String.class);
+		
+		return response;
+	}
+	
+	
+	/**
+	 * http://localhost:8080/service2/v2/
+	 * @return
+	 */
+	@GetMapping("/v2")
+	public String iAmDependentOnService2() {
+		
+		// OLD APPROACH
+		String url = "http://SERVICE1/service1/";
+		String response = restTemplate.getForObject(url, String.class);
+		
+		return response;
+	}
+	
+	
+	@GetMapping("/v3")
+	public String iAmDependentOnService3() {
+		
+		// OLD APPROACH
+		String url = "lb://SERVICE1/service1/";
+		String response = restTemplate.getForObject(url, String.class);
+		
+		return response;
 	}
 
 }
